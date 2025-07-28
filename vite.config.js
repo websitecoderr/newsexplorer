a9bc199d00f83c284337c40816f3483bc2e493d7
Alfa <opportunities91@gmail.com>
2025-07-28 09:55:08 -0700

    project update

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,5 @@ export default {
   define: {
     'import.meta.env.VITE_NEWS_API_KEY': JSON.stringify(process.env.VITE_NEWS_API_KEY || '0ae22c0a6e884ff99fdc04c8959d80e3'),
   },
+  base: "/newsexplorer/"
 };
